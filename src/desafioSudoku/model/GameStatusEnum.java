18d1f0e8458948ec0574d795cc7dcf9dfa8439d0
Heitor <vilaverde88@gmail.com>
2025-03-26 10:18:24 -0300

    Initial commit

---
+++ src/desafioSudoku/model/GameStatusEnum.java
@@ -0,0 +1,17 @@
+package desafioSudoku.model;
+
+public enum GameStatusEnum {
+    NON_STARTED("Não Iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("Completo");
+
+    private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
